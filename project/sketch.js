057ab42c96969cf72cea6977ec86c1c9ac81e77c
sloane <103333297+smv307@users.noreply.github.com>
2025-01-14 02:31:53 +0000

    fixed errors

--- project/sketch.js
+++ project/sketch.js
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
 let video;
 let bodyPose;
 let poses = [];
-const eyes = [];
-let connections;
+
 const randomizeButton = document.getElementById("randomize");
-let randomEye;
+const eyes = [];
+let currentEye;
 
 function preload() {
   bodyPose = ml5.bodyPose();
 }
-
-
 
 function setup() {
   createCanvas(640, 480);
@@ -21,23 +18,21 @@ function setup() {
   video.size(640, 480);
   video.hide();
 
+  //initial randomization
+  currentEye = eyes[Math.floor(Math.random() * eyes.length)];
+
   // start detecting poses in the webcam video
   bodyPose.detectStart(video, gotPoses);
-  // get the skeleton connection information
-  connections = bodyPose.getSkeleton();
-
-  // initial randomization
-  randomEye();
 }
 
 function draw() {
-  // draw the webcam video
-  image(video, 0, 0, width, height);
-
   //check if button clicked, if so, randomize
   randomizeButton.addEventListener('click', () => {
-    randomEye = eyes[Math.floor(Math.random() * eyes.length)];
+    currentEye = eyes[Math.floor(Math.random() * eyes.length)];
   });
+
+  // draw the webcam video
+  image(video, 0, 0, width, height);
 
   // draw eyes
   for (let i = 0; i < poses.length; i++) {
@@ -48,7 +43,7 @@ function draw() {
       if (keypoint.confidence > 0.1) {
         fill(0, 255, 0);
         noStroke();
-        randomEye(keypoint.x, keypoint.y);
+        currentEye(keypoint.x, keypoint.y);
       }
     }
   }
@@ -58,49 +53,4 @@ function draw() {
 function gotPoses(results) {
   // save the output to the poses variable
   poses = results;
-=======
-let faceMesh;
-let video;
-let options = { maxFaces: 1, refineLandmarks: false, flipped: false };
-
-let faces = []; // all faces detected, facepoints for each
-
-function preload() {
-    faceMesh = ml5.faceMesh(options);
-}
-
-function setup() {
-    createCanvas(640, 480);
-
-    // create webcam video and hide it
-    video = createCapture(VIDEO);
-    video.size(640, 480);
-    video.hide();
-
-    faceMesh.detectStart(video, gotFaces); // begin to detect faces with webcam 
-}
-
-function draw(){
-    image(video, 0, 0, width, height); // draw webcam video
-/*
-    if (faces.length > 0 && faces[0].eyes){
-        fill(140, 158, 189)
-    }
-*/
-    // draw all the tracked face points
-    for (let i = 0; i < faces.length; i++) {
-        let face = faces[i];
-        for (let j = 0; j < face.keypoints.length; j++) {
-            let keypoint = face.keypoints[j];
-            fill(180, 143, 194);
-            noStroke();
-            circle(keypoint.x, keypoint.y, 5);
-        }
-    }
-}
-
-// when faceMesh outputs data
-function gotFaces(results) {
-    faces = results; // save the output to the faces variable
->>>>>>> 480a5ec511622c9f08b6c61a88ebadb9f83f8438
 }
